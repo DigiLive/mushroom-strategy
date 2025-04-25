afa6b88bfd0e5c61925eaa7f721b8a1b22357f79
DigiLive <github@digilive.nl>
2025-04-25 10:19:20 +0200

    Add support for Stateful scenes
    Add changing the icon color of the Scene Card depending on the related
    stateful scene entity, if available.
    The default color will now be `disabled`.
    
    Closes #190

--- src/cards/SceneCard.ts
+++ src/cards/SceneCard.ts
@@ -3,6 +3,7 @@
 import { Registry } from '../Registry';
 import { EntityRegistryEntry } from '../types/homeassistant/data/entity_registry';
 import { EntityCardConfig } from '../types/lovelace-mushroom/cards/entity-card-config';
+import { TemplateCardConfig } from '../types/lovelace-mushroom/cards/template-card-config';
 import { isCallServiceActionConfig, isCallServiceActionTarget } from '../types/strategy/strategy-generics';
 import AbstractCard from './AbstractCard';
 
@@ -13,11 +14,11 @@ import AbstractCard from './AbstractCard';
  */
 class SceneCard extends AbstractCard {
   /** Returns the default configuration object for the card. */
-  static getDefaultConfig(): EntityCardConfig {
+  static getDefaultConfig(): TemplateCardConfig {
     return {
-      type: 'custom:mushroom-entity-card',
+      type: 'custom:mushroom-template-card',
       icon: 'mdi:palette',
-      icon_color: 'blue',
+      icon_color: 'disabled',
       tap_action: {
         action: 'call-service',
         perform_action: 'scene.turn_on',
@@ -37,7 +38,11 @@ class SceneCard extends AbstractCard {
   constructor(entity: EntityRegistryEntry, customConfiguration?: EntityCardConfig) {
     super(entity);
 
+    const sceneName = entity.entity_id.split('.').pop();
     const configuration = SceneCard.getDefaultConfig();
+
+    // Initialize the default configuration.
+    configuration.primary = entity.name ?? entity.original_name ?? '?';
 
     if (
       isCallServiceActionConfig(configuration.tap_action) &&
@@ -46,6 +51,16 @@ class SceneCard extends AbstractCard {
       configuration.tap_action.target.entity_id = entity.entity_id;
     }
     configuration.icon = Registry.hassStates[entity.entity_id]?.attributes.icon ?? configuration.icon;
+
+    // Stateful Scenes support. (https://github.com/hugobloem/stateful_scenes)
+    configuration.icon_color = `
+        {% set state = states('switch.${sceneName}_stateful_scene') %}
+          {% if state == 'on' %}
+            blue
+          {% else %}
+            disabled
+          {% endif %}
+        `;
 
     this.configuration = { ...this.configuration, ...configuration, ...customConfiguration };
   }
