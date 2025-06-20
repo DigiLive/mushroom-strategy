d5349fa8d54973e41e441ed7aa2339702f45be94
DigiLive <github@digilive.nl>
2025-06-20 16:36:44 +0200

    Refactor notification message

--- src/notifications.ts
+++ src/notifications.ts
@@ -5,7 +5,7 @@ export const NOTIFICATIONS = [
     message:
       '## Deprecation Notice\n' +
       'As of v3.0.0, chips are replaced by badges.\n' +
-      'From this version on, you must rename all `chip` or `chips` references and settings in your YAML configuration.\n' +
+      'From that version on, you must rename all `chip` or `chips` references and settings in your YAML configuration.\n' +
       'The [documentation](https://digilive.github.io/mushroom-strategy/options/home-view-options/) will be updated accordingly.',
     fromVersion: '2.3.5',
     toVersion: '3.0.0',
