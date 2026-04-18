dc02fdf5ae391c925a3037c8da17fbef6238915d
DigiLive <github@digilive.nl>
2026-04-18 08:02:53 +0200

    Bump version to v3.1.0

--- src/mushroom-strategy.ts
+++ src/mushroom-strategy.ts
@@ -296,7 +296,7 @@ class MushroomStrategy extends HTMLTemplateElement {
 
 customElements.define('ll-strategy-mushroom-strategy', MushroomStrategy);
 
-const STRATEGY_VERSION = 'v3.0.1';
+const STRATEGY_VERSION = 'v3.1.0';
 console.info(
   '%c Mushroom Strategy %c '.concat(STRATEGY_VERSION, ' '),
   'color: white; background: coral; font-weight: 700;',
